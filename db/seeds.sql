28f3f8efa45b75a3ecd6ce10f5be80a69ddf5457
Thomas Lewis <tplewis@gmail.com>
2019-08-09 17:39:48 -0400

    seeds.sql added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Select the burgers_db
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("turkey burger", false), ("black bean burger", false), ("tofu burger", false);
